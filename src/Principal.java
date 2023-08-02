ae6637a8b6f0e185252e45ff7659cccfb6b8177d
Salma Souza <salma.souzza@gmail.com>
2023-08-02 16:32:00 -0300

    adicionado funcionarios

--- src/Principal.java
+++ src/Principal.java
@@ -13,11 +13,16 @@ public class Principal {
         List<Funcionario> funcionarios = new ArrayList<>();
 
         // 3.1 - Inserir funcionários
-        funcionarios.add(new Funcionario(1,"João", new BigDecimal("2500.00"), "Gerente", LocalDate.of(1985, 5, 10)));
-        funcionarios.add(new Funcionario(2,"Maria", new BigDecimal("1800.00"), "Assistente", LocalDate.of(1990, 9, 25)));
-        funcionarios.add(new Funcionario(3,"Pedro", new BigDecimal("3000.00"), "Analista", LocalDate.of(1982, 3, 15)));
-        funcionarios.add(new Funcionario(4,"Ana", new BigDecimal("2200.00"), "Assistente", LocalDate.of(1992, 11, 5)));
-        funcionarios.add(new Funcionario(5,"José", new BigDecimal("3500.00"), "Gerente", LocalDate.of(1978, 8, 2)));
+        funcionarios.add(new Funcionario(2,"Maria", new BigDecimal("2009.44"), "Operador", LocalDate.of(2000, 10, 18)));
+        funcionarios.add(new Funcionario(1,"João", new BigDecimal("2284.38"), "Operador", LocalDate.of(1990, 05, 12)));
+        funcionarios.add(new Funcionario(3,"Caio", new BigDecimal("9836.14"), "Coordenador", LocalDate.of(1961, 05, 02)));
+        funcionarios.add(new Funcionario(4,"Miguel", new BigDecimal("19119.88"), "Diretor", LocalDate.of(1988, 10, 14)));
+        funcionarios.add(new Funcionario(5,"Alice", new BigDecimal("2234.68"), "Recepcionista", LocalDate.of(1995, 01, 05)));
+        funcionarios.add(new Funcionario(6,"Heitor", new BigDecimal("1582.72"), "Operador", LocalDate.of(1999, 11, 19)));
+        funcionarios.add(new Funcionario(7,"Arthur", new BigDecimal("4071.84"), "Contador", LocalDate.of(1993, 03, 31)));
+        funcionarios.add(new Funcionario(8,"Laura", new BigDecimal("3017.45"), "Gerente", LocalDate.of(1994, 07, 8)));
+        funcionarios.add(new Funcionario(9,"Heloísa", new BigDecimal("1606.85"), "Eletricista", LocalDate.of(2003, 05, 24)));
+        funcionarios.add(new Funcionario(10,"Helena", new BigDecimal("2799.93"), "Gerente", LocalDate.of(1996, 9, 02)));
 
         // 3.2 - Remover funcionário "João" da lista
         funcionarios.removeIf(funcionario -> funcionario.getNome().equals("João"));
@@ -30,12 +35,12 @@ public class Principal {
         funcionarios.forEach(funcionario -> funcionario.setSalario(funcionario.getSalario().multiply(new BigDecimal("1.1"))));
 
         // 3.5 - Agrupar funcionários por função em um MAP
-        Map<String, List<Funcionario>> funcionariosPorFuncao = funcionarios.stream()
+        Map<String, List<Funcionario>> funcionariosFuncao = funcionarios.stream()
                 .collect(Collectors.groupingBy(Funcionario::getFuncao));
 
         // 3.6 - Imprimir funcionários agrupados por função
         System.out.println("\nFuncionários agrupados por função:");
-        funcionariosPorFuncao.forEach((funcao, listaFuncionarios) -> {
+        funcionariosFuncao.forEach((funcao, listaFuncionarios) -> {
             System.out.println("Função: " + funcao);
             listaFuncionarios.forEach(System.out::println);
         });
@@ -79,10 +84,17 @@ public class Principal {
         // 3.12 - Imprimir quantos salários mínimos ganha cada funcionário
         BigDecimal salarioMinimo = new BigDecimal("1212.00");
         System.out.println("\nSalários em múltiplos do salário mínimo:");
-        funcionarios.forEach(funcionario -> {
-            BigDecimal multiplicador = funcionario.getSalario().divide(salarioMinimo, 2, BigDecimal.ROUND_DOWN);
-            System.out.println(funcionario.getNome() + " ganha " + multiplicador + " salários mínimos.");
+        // funcionarios.forEach(funcionario -> {
+        //     BigDecimal multiplicador = funcionario.getSalario().divide(salarioMinimo, 2, BigDecimal.ROUND_DOWN);
+        //     System.out.println(funcionario.getNome() + " ganha " + multiplicador + " salários mínimos.");
+        // });
+
+        funcionarios.forEach(funcionario ->{
+                   BigDecimal[]  salarioQnt = funcionario.getSalario().divideAndRemainder(salarioMinimo);
+                   System.out.println(salarioQnt);
         });
+
+        
     }
 
 }
